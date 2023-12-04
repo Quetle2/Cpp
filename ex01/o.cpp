2be520d84bccfa273ea524d3da9d5cd1fe7c94d6
Miguel Alexandre Andrade Arada <miandrad@c2r2s8.42lisboa.com>
2023-12-04 18:17:51 +0000

    a

--- ex01/o.cpp
+++ ex01/o.cpp
@@ -1,31 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   o.cpp                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: miandrad <miandrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/04 17:02:49 by miandrad          #+#    #+#             */
-/*   Updated: 2023/12/04 17:14:02 by miandrad         ###   ########.fr       */
+/*   Created: 2023/12/04 17:02:46 by miandrad          #+#    #+#             */
+/*   Updated: 2023/12/04 17:07:53 by miandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-
-int	main(int argc, char **argv){
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	for (int i = 1;argv[i] && argv[0];i++)
-	{
-		for (int j = 0;argv[i][j];j++)
-		{
-			if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
-			{
-				char a = argv[i][j] - 32;
-				std::cout << a;
-			}
-			else{std::cout << argv[i][j];}
-		}
-	}
-	std::cout << "\n";
-}
