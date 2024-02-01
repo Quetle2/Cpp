beaf8d09435e2c2077b7c7a78efc12cf0a6216f5
Miguel Alexandre Andrade Arada <miandrad@c2r4s1.42lisboa.com>
2024-02-01 16:50:42 +0000

    ta crazy

---
+++ ex02/Account.cpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miandrad <miandrad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/01 15:24:26 by miandrad          #+#    #+#             */
+/*   Updated: 2024/02/01 15:53:43 by miandrad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+t::Account(int initial_deposit){
+	
+}
+
+t::~Account(void){
+
+}
+
+static int	t::getNbAccounts(void){
+	
+}
+
+static int	t::getTotalAmount(void){
+	
+}
+
+static int	t::getNbDeposits(void){
+	
+}
+
+static int	t::getNbWithdrawals(void){
+	
+}
+
+static void	t::displayAccountsInfos(void){
+	
+}
+
+void	t::makeDeposit(int deposit){
+	
+}
+
+bool	t::makeWithdrawal(int withdrawal){
+	
+}
+
+int		t::checkAmount(void)const{
+	
+}
+
+void	t::displayStatus(void)const{
+	
+}
+
+static void	t::_displayTimestamp(void){
+	
+}
